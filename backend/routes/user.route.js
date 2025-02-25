0d28f198ec5a472ff9c24f51985010433504bbd1
BhanuPrakash43 <bhanu742630@gmail.com>
2025-02-26 01:54:16 +0530

    add user routes for registration, login, logout, and token refresh

---
+++ backend/routes/user.route.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
+
+const userRouter = express.Router();
+
+userRouter.post(
+    "/register",
+    upload.fields([{ name: "profile", maxCount: 1 }]),
+    (req, res, next) => {
+        next();
+    },
+    registerUser
+);
+
+userRouter.route("/login").post(loginUser);
+
+// secured routes
+userRouter.route("/logout").post(verifyJWT, logoutUser);
+userRouter.route("/refresh-token").post(refreshAccessToken);
+
+export default userRouter;
