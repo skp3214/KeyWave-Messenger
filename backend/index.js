b413fa715397296d26d8df6475a3b702c7f05cab
BhanuPrakash43 <bhanu742630@gmail.com>
2025-02-26 01:54:34 +0530

    initialize Express server with CORS, body parsing, cookie parsing, and user routes

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./db/index.js";
+import userRouter from "./routes/user.route.js";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+dotenv.config({ path: "./.env" });
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on: http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed!", err);
+    });
