fb0d307c74b0b311bcf7817bc4a91f4969db6f36
Sachin Prajapati <skprajapati3214@gmail.com>
2025-03-02 22:48:31 +0530

    add online users and chat routes to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,33 @@ import {
     createRoutesFromElements,
     Route,
     RouterProvider,
-} from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import { Toaster } from "react-hot-toast";
-import AuthContextProvider from "./contexts/AuthContextProvider";
-
-const router = createBrowserRouter(
+  } from "react-router-dom";
+  import Layout from "./components/Layout";
+  import Home from "./pages/Home";
+  import { Toaster } from "react-hot-toast";
+  import AuthContextProvider from "./contexts/AuthContextProvider";
+  import OnlineUsers from "./pages/OnlineUsers";
+  import Chat from "./pages/chat";
+  
+  const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-            </Route>
+      <Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="online-users" element={<OnlineUsers />} />
+          <Route path="chat/:username" element={<Chat />} />
         </Route>
+      </Route>
     )
-);
-
-function App() {
+  );
+  
+  function App() {
     return (
-        <AuthContextProvider>
-            <Toaster />
-            <RouterProvider router={router} />
-        </AuthContextProvider>
+      <AuthContextProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     );
-}
-export default App;
+  }
+  
+  export default App;
